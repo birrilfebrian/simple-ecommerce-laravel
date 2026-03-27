444c61d59a05adede16e108615c454af5f397671
birrilfebrian <55568576+birrilfebrian@users.noreply.github.com>
2026-03-27 18:57:06 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,9 +16,11 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Sasa',
+            'email' => 'sasa@app.com',
+            'level' => 'admin',
+            'credits' => 9999
+        ]);
     }
 }
